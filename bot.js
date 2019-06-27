a85d768f668ecca3870849da5e7e6ce7b6077064
Roman <r89534217610@gmail.com>
2019-06-27 19:59:21 +0300

    resolve #1

--- bot.js
+++ bot.js
@@ -3,39 +3,122 @@ const config = require('./config.json');
 
 const client = new Discord.Client();
 
+var guildMembers = [];//todo: move it somewhere
+
+function commandSplit(command, limit) {//using split splice push would result in losing multiple spaces if any
+    let result = [];
+    let length = 0;
+
+    let str = '';
+
+    for (var i = 0; i < command.length; i += 1) {
+        if (command[i] == ' ' && str != '') {//todo: add isLongArgument var that signals that arg may contain spaces like `query`
+            result.push(str);
+            length += 1;
+            str = '';
+
+            while (command[i += 1] == ' ') { }
+
+            if (length == limit - 1) {//leave 1 for the last element
+                break;
+            }
+            else {
+                i -= 1;
+            }
+        }
+        else {
+            str += command[i];
+        }
+    }
+
+    if (str != '') {
+        result.push(str);
+    }
+    else {
+        for (; i < command.length; i += 1) {
+            str += command[i];
+        }
+
+        result.push(str);
+    }
+
+    return result;
+}
+
 client.on('ready', () => {
     console.log('I am ready!');
-    client.user.setActivity('YOU', { type: "LISTENING" });
+    client.user.setActivity('', { type: 'WATCHING' });
 });
 
 client.on('message', message => {
+
+    let message_content = message.content.trim();
+
+    if (message_content.startsWith(config.prefix)) {
+        let parts = commandSplit(message_content.substring(config.prefix.length), 2);
+
+        switch (parts[0]) {
+            case 'q':
+                let arg = parts[1];
+                let query = '';
+                if (arg[0] == '`') {
+                    if (arg[arg.length - 1] == '`') {
+                        query = arg.substring(1, arg.length - 1);
+                    }
+                    else {
+                        //failed initial validation
+                        message.channel.sendMessage('invalid query');
+                    }
+                }
+                else {
+                    query = arg;
+                }
+
+                //todo: validate query
+
+                guildMembers = message.guild.members;//todo: use fetchMembers()
+
+                let query_tree = buildQueryTree(query);
+                let res = executeQuery(query_tree, null, -1);
+
+                let msg = '';
+                for (let id of res) {
+                    msg += `<@!${id}> `;
+                }
+
+                message.channel.sendMessage(res.length + ' | ' + msg);
+
+                break;
+
+            default:
+                break;
+        }
+    }
+    /*
     if (message.author.id == '396787552775831552') {
         message.channel.send('shut up biba');
     }
 
     if (message.content.startsWith('f')) {
         var sas = message.content.split(' ')[1];
-        /*
-                var str = '';
-        
-                for (var [snowflake, member] of message.guild.members) {
-                    str += (member.nickname || '') + '|' + member.user.username + '\n';
-                }
-        
-                message.channel.send(str);
-                */
+
+        var str = '';
+
+        for (var [snowflake, member] of message.guild.members) {
+            str += (member.nickname || '') + '|' + member.user.username + '\n';
+        }
+
+        message.channel.send(str);
+
         message.channel.send('\\' + sas);
     }
 
     if (message.content === 'ping') {
         message.channel.send('pong');
     }
-
+*/
     if (message.content === 'exit') {
-        if (message.author.id == '396787552775831552') {
-            message.channel.send('no exit this time! yo toped ass mafaq');
-        }
-        else {
+        if (message.author.id == '224099990455189504') {
             message.channel.send('k');
 
             client.destroy().then(() => {
@@ -44,12 +127,10 @@ client.on('message', message => {
         }
     }
 
-
 });
 
-//client.login(config.token);
-
-var test_users = [
+/*
+var guildMembers = [
     {
         'username': 'sas',
         'nickname': 'bob',
@@ -88,7 +169,14 @@ var test_users = [
 var query1 = 'username=sas';
 var query2 = 'username=bab&role=vev';
 var query3 = '(username=sas|username=sos)|(role=bobs|role=vev)&(nickname=sos|nickname=ses)';//u  - username, n - nickname, r = role
-var query = 'role=vev&username=bab|username=sas&nickname=bob';
+var query4 = 'role=vev&username=bab|username=sas&nickname=bob';
+*/
+const EQueryFlags = {
+    NONE: 0,
+    INVERTED: 1 << 0,
+    PARTIAL: 1 << 1,
+    CASESENSITIVE: 1 << 2
+}
 
 class Node {
     constructor(tag = null, val = null, operator = 0) {
@@ -102,133 +190,137 @@ class Node {
     }
 }
 
-var tag = '';
-var val = '';
+function buildQueryTree(query) {
+    var tag = '';
+    var val = '';
 
-var is_tag = true;
+    var is_tag = true;
 
-var root_node = new Node();
-var cur = root_node;
+    var root_node = new Node();
+    var cur = root_node;
 
-var balance = 0;
+    var balance = 0;
 
-for (let i = 0; i < query.length; i += 1) {
-    let c = query[i];
+    for (let i = 0; i < query.length; i += 1) {
+        let c = query[i];
 
-    if (c == '(') {
-        balance += 1;
+        if (c == '(') {
+            balance += 1;
 
-        //higher (technically lower) priority
-        cur.low = new Node();
-        cur.low.high = cur;
+            //higher (technically lower) priority
+            cur.low = new Node();
+            cur.low.high = cur;
 
-        cur = cur.low;
+            cur = cur.low;
 
-        continue;
-    }
-
-    if (c == ')') {
-        balance -= 1;
-
-        cur.tag = tag;
-        cur.val = val;
-        cur.operator = 0;
-
-        tag = '';
-        val = '';
-
-        is_tag = true;
-
-        //skip for multiple closing brackets
-        while (query[i] == ')') {
-            cur = cur.high;
-            i += 1;
+            continue;
         }
 
-        if (i < query.length) {
-            c = query[i];
+        if (c == ')') {
+            balance -= 1;
 
-            if (c == '&') {
-                //push double priority for brackets and AND operator
-                let low2 = cur.low;
+            cur.tag = tag;
+            cur.val = val;
+            cur.operator = 0;
 
-                cur.low = new Node();
-                cur.low.high = cur;
+            tag = '';
+            val = '';
 
-                cur = cur.low;
-                cur.low = low2;
-                cur.low.high = cur;
+            is_tag = true;
 
-                let cur2 = cur.low;
+            //skip for multiple closing brackets
+            while (query[i] == ')') {
+                cur = cur.high;
+                i += 1;
+            }
 
-                while (cur2.next != null) {
-                    cur2 = cur2.next;
+            if (i < query.length) {
+                c = query[i];
+
+                if (c == '&') {
+                    //push double priority for brackets and AND operator
+                    let low2 = cur.low;
+
+                    cur.low = new Node();
+                    cur.low.high = cur;
+
+                    cur = cur.low;
+                    cur.low = low2;
+                    cur.low.high = cur;
+
+                    let cur2 = cur.low;
+
+                    while (cur2.next != null) {
+                        cur2 = cur2.next;
+                    }
+
+                    cur2.high = cur;
+
+                    cur.operator = c;
+                }
+                else if (c == '|') {
+                    cur.operator = c;
                 }
 
-                cur2.high = cur;
+                cur.next = new Node();
+                cur.next.high = cur.high;
+                cur = cur.next;
+            }
 
-                cur.operator = c;
-            }
-            else if (c == '|') {
-                cur.operator = c;
-            }
+            continue;
+        }
+
+        if (c == '=') {
+            is_tag = false;
+            continue;
+        }
+
+        if (c == '&') {
+            cur.low = new Node(tag, val, c);
+            cur.low.high = cur;
+
+            cur = cur.low;
+            cur.next = new Node();
+            cur.next.high = cur.high;
+            cur = cur.next;
+
+            tag = '';
+            val = '';
+
+            is_tag = true;
+        }
+        else if (c == '|') {
+            cur.tag = tag;
+            cur.val = val;
+            cur.operator = c;
 
             cur.next = new Node();
             cur.next.high = cur.high;
             cur = cur.next;
-        }
 
-        continue;
-    }
+            tag = '';
+            val = '';
 
-    if (c == '=') {
-        is_tag = false;
-        continue;
-    }
-
-    if (c == '&') {
-        cur.low = new Node(tag, val, c);
-        cur.low.high = cur;
-
-        cur = cur.low;
-        cur.next = new Node();
-        cur.next.high = cur.high;
-        cur = cur.next;
-
-        tag = '';
-        val = '';
-
-        is_tag = true;
-    }
-    else if (c == '|') {
-        cur.tag = tag;
-        cur.val = val;
-        cur.operator = c;
-
-        cur.next = new Node();
-        cur.next.high = cur.high;
-        cur = cur.next;
-
-        tag = '';
-        val = '';
-
-        is_tag = true;
-    }
-    else {
-        if (is_tag) {
-            tag += c;
+            is_tag = true;
         }
         else {
-            val += c;
+            if (is_tag) {
+                tag += c;
+            }
+            else {
+                val += c;
+            }
         }
     }
+
+    cur.tag = tag || null;
+    cur.val = val || null;
+    cur.operator = 0;
+
+    //cur = root_node;
+
+    return root_node;
 }
-
-cur.tag = tag || null;
-cur.val = val || null;
-cur.operator = 0;
-
-cur = root_node;
 
 function executeQuery(node, filtered_prev, operator) {
 
@@ -238,10 +330,42 @@ function executeQuery(node, filtered_prev, operator) {
         filtered = executeQuery(node.low, null, -1);
     }
     else {
-        for (let user of test_users) {
-            if (user[node.tag] == node.val) {//todo: add support for flags
-                filtered.push(user);
-            }
+        node.tag = node.tag.toLowerCase();
+        node.val = node.val.toLowerCase();
+
+        switch (node.tag) {//todo: add support for flags
+            case 'nickname':
+            case 'n':
+                for (let [, member] of guildMembers) {
+                    if (member.nickname.toLowerCase() == node.val) {
+                        filtered.push(member.id);
+                    }
+                }
+                break;
+            case 'username':
+            case 'u':
+                for (let [, member] of guildMembers) {
+                    if (member.user.username.toLowerCase() == node.val) {
+                        filtered.push(member.id);
+                    }
+                }
+                break;
+            case 'role':
+            case 'r':
+                for (let [, member] of guildMembers) {
+                    for (let [snowflake, role] of member.roles) {
+                        if (role.name.toLowerCase() == node.val) {
+                            filtered.push(member.id);
+                            break;//if there are identical roles, this will prevent adding a user multiple times if they got them both
+                        }
+                    }
+                }
+                break;
+            case 'bot':
+            case 'b':
+                break;
+            default:
+                break;
         }
     }
 
@@ -276,4 +400,6 @@ function executeQuery(node, filtered_prev, operator) {
     }
 }
 
-console.log(executeQuery(root_node, null, -1));
+
+client.login(config.token);
+//console.log(executeQuery(root_node, null, -1));
